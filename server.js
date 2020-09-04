19b869976d400c0671958fa96113f0d297d60e27
Larraine Gonzales <larrainegonzales@hotmail.com>
2020-09-03 21:15:30 -0500

    working code! Do not break

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
-// Dependencies
-var fs = require("fs");
 var express = require("express");
-var path = require("path");
-
-// Express App
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-// Sets up the Express App
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// Routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
-
-
-// Server Listener
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
+});
